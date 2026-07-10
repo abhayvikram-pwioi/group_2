44278add27c1a4c532272db660005c6436fefdf3
Ishika Gangwar <ishugang27june2006@gmail.com>
2026-07-10 13:55:51 +0530

    admin dashboard completed

---
+++ Event listing/admin.js
@@ -0,0 +1,287 @@
+/*==========================
+      EDIT VARIABLES
+==========================*/
+
+let currentRow = null;
+
+let isEditMode = false;
+/*==========================
+      ADD EVENT MODAL
+==========================*/
+
+const addBtn = document.querySelector(".add-btn");
+
+const eventModal = document.getElementById("eventModal");
+
+const closeEvent = document.querySelector(".close-event");
+
+addBtn.addEventListener("click",()=>{
+
+    eventModal.style.display="flex";
+
+});
+
+closeEvent.addEventListener("click",()=>{
+
+    eventModal.style.display="none";
+
+});
+
+window.addEventListener("click",(e)=>{
+
+    if(e.target===eventModal){
+
+        eventModal.style.display="none";
+
+    }
+
+});
+
+/*==========================
+      ADD EVENT
+==========================*/
+
+const eventForm = document.getElementById("eventForm");
+
+const tableBody = document.getElementById("eventTableBody");
+
+eventForm.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const name = document.getElementById("eventName").value;
+
+    const date = document.getElementById("eventDate").value;
+
+    const location = document.getElementById("eventLocation").value;
+
+    const newRow = document.createElement("tr");
+
+    newRow.innerHTML = `
+
+        <td>${name}</td>
+
+        <td>${date}</td>
+
+        <td>${location}</td>
+
+        <td>
+
+            <button class="edit-btn">
+
+                <i class="fa-solid fa-pen"></i>
+
+                Edit
+
+            </button>
+
+            <button class="delete-btn">
+
+                <i class="fa-solid fa-trash"></i>
+
+                Delete
+
+            </button>
+
+        </td>
+
+    `;
+
+    eventForm.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    const name = document.getElementById("eventName").value;
+
+    const date = document.getElementById("eventDate").value;
+
+    const location = document.getElementById("eventLocation").value;
+
+    if (isEditMode) {
+
+        currentRow.cells[0].innerText = name;
+
+        currentRow.cells[1].innerText = date;
+
+        currentRow.cells[2].innerText = location;
+
+        isEditMode = false;
+
+        currentRow = null;
+
+        saveBtn.innerText = "Create Event";
+
+    }
+
+    else {
+
+        const newRow = document.createElement("tr");
+
+        newRow.innerHTML = `
+
+            <td>${name}</td>
+
+            <td>${date}</td>
+
+            <td>${location}</td>
+
+            <td>
+
+                <button class="edit-btn">
+
+                    <i class="fa-solid fa-pen"></i>
+
+                    Edit
+
+                </button>
+
+                <button class="delete-btn">
+
+                    <i class="fa-solid fa-trash"></i>
+
+                    Delete
+
+                </button>
+
+            </td>
+
+        `;
+
+        tableBody.appendChild(newRow);
+
+    }
+
+    eventModal.style.display = "none";
+
+    eventForm.reset();
+
+});
+
+});
+
+/*==========================
+      DELETE EVENT
+==========================*/
+
+document.addEventListener("click", function(e){
+
+    if(e.target.closest(".delete-btn")){
+
+        if(confirm("Delete this event?")){
+
+            e.target.closest("tr").remove();
+
+        }
+
+    }
+
+});
+
+/*==========================
+      EDIT EVENT
+==========================*/
+
+document.addEventListener("click", function(e){
+
+    if(e.target.closest(".edit-btn")){
+
+       /*==========================
+      EDIT EVENT
+==========================*/
+const saveBtn = document.getElementById("saveBtn");
+
+document.addEventListener("click", function (e) {
+
+    if (e.target.closest(".edit-btn")) {
+
+        currentRow = e.target.closest("tr");
+
+        isEditMode = true;
+
+        eventModal.style.display = "flex";
+
+        document.getElementById("eventName").value =
+        currentRow.cells[0].innerText;
+
+        document.getElementById("eventDate").value =
+        currentRow.cells[1].innerText;
+
+        document.getElementById("eventLocation").value =
+        currentRow.cells[2].innerText;
+
+        saveBtn.innerText = "Update Event";
+
+    }
+
+});
+    }
+
+});
+
+/*==========================
+      SIDEBAR NAVIGATION
+==========================*/
+
+const dashboardLink = document.getElementById("dashboardLink");
+
+const eventsLink = document.getElementById("eventsLink");
+
+const dashboardSection = document.getElementById("dashboardSection");
+
+const eventsSection = document.getElementById("eventsSection");
+
+
+dashboardLink.addEventListener("click",()=>{
+
+    dashboardSection.scrollIntoView({
+
+        behavior:"smooth"
+
+    });
+
+});
+
+
+eventsLink.addEventListener("click",()=>{
+
+    eventsSection.scrollIntoView({
+
+        behavior:"smooth"
+
+    });
+
+});
+
+/*==========================
+        LOGOUT
+==========================*/
+
+const logoutBtn=document.getElementById("logoutBtn");
+
+logoutBtn.addEventListener("click",()=>{
+
+    const answer=confirm("Are you sure you want to logout?");
+
+    if(answer){
+
+        window.location.href="home.html";
+
+    }
+
+});
+
+dashboardLink.addEventListener("click",()=>{
+
+    dashboardLink.classList.add("active");
+
+    eventsLink.classList.remove("active");
+
+});
+
+eventsLink.addEventListener("click",()=>{
+
+    eventsLink.classList.add("active");
+
+    dashboardLink.classList.remove("active");
+
+});
